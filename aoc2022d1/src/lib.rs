3b588953df828d548f7f7ff1489670e28e1e2647
Felipe Balbi <felipe@balbi.sh>
2023-04-12 08:53:23 +0300

    nom-playground: aoc2022d1: add clap
    Signed-off-by: Felipe Balbi <felipe@balbi.sh>

--- aoc2022d1/src/lib.rs
+++ aoc2022d1/src/lib.rs
@@ -1,3 +1,4 @@
+use clap::Parser;
 use nom::{
     character::complete::{self, newline},
     multi::{many1, separated_list1},
@@ -6,6 +7,33 @@ use nom::{
 use std::cmp::Reverse;
 
 type Calorie = u32;
+
+#[derive(Parser, Debug)]
+#[command(
+    name = "aoc2022d1",
+    author = "Felipe Balbi <felipe@balbi.sh>",
+    about = "Advent of Code 2022 Day 1",
+    version = "0.1.0"
+)]
+pub struct Cli {
+    #[arg(short = '1', long = "part1", help = "Run part 1.")]
+    part1: bool,
+
+    #[arg(short = '2', long = "part2", help = "Run part 2.")]
+    part2: bool,
+}
+
+impl Cli {
+    pub fn run(&self, input: &str) {
+        if self.part1 {
+            println!("Part 1: {}", part1(input));
+        }
+
+        if self.part2 {
+            println!("Part 2: {}", part2(input));
+        }
+    }
+}
 
 fn parse_calorie(input: &str) -> IResult<&str, Calorie> {
     let (input, calories) = separated_list1(newline, complete::u32)(input)?;
@@ -18,13 +46,13 @@ fn parse_calories(input: &str) -> IResult<&str, Vec<Calorie>> {
     separated_list1(many1(newline), parse_calorie)(input)
 }
 
-pub fn part1(input: &str) -> u32 {
+fn part1(input: &str) -> u32 {
     let (_, result) = parse_calories(input).unwrap();
 
     result.into_iter().max().unwrap()
 }
 
-pub fn part2(input: &str) -> u32 {
+fn part2(input: &str) -> u32 {
     let (_, mut result) = parse_calories(input).unwrap();
 
     result.sort_by_key(|k| Reverse(*k));
--- aoc2022d1/src/main.rs
+++ aoc2022d1/src/main.rs
@@ -1,9 +1,8 @@
-use aoc2022d1::{part1, part2};
+use aoc2022d1::Cli;
+use clap::Parser;
 use std::fs;
 
 fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
-
-    println!("Part 1: {}", part1(&input));
-    println!("Part 2: {}", part2(&input));
+    Cli::parse().run(&input);
 }
